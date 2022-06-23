dcfc84ffb3338e5609d3509649bb0a3968b8104e
danielashjari <70367201+danielashjari@users.noreply.github.com>
2022-06-23 13:49:37 +0430

    create and style toggle + correct the colors

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,20 @@
-function darkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-};
+const checkbox = document.getElementById('checkbox');
+checkbox.addEventListener('click', checkMode);
+
+function checkMode() {
+    if (checkbox.checked) {
+        darkModeOn();
+    } else {
+        darkModeOff();
+    }
+}
+
+function darkModeOn() {
+    document.body.classList.add('dark-mode');
+}
+
+function darkModeOff() {
+    document.body.classList.remove('dark-mode');
+}
+
+
