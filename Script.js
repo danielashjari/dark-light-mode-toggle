582fcab62139b57e2f5a9d552c7d7be099412c08
danielashjari <70367201+danielashjari@users.noreply.github.com>
2022-07-27 21:32:27 +0430

    step 2: JS fix, toggle checked on pcs=dark

--- Script.js
+++ Script.js
@@ -1,2 +1,2 @@
-"use strict";var CHECKBOX=document.getElementById("checkbox");CHECKBOX.addEventListener("change",(function(e){e.target.checked?document.documentElement.setAttribute("data-theme","dark"):document.documentElement.setAttribute("data-theme","light")}));
+"use strict";var CHECKBOX=document.getElementById("checkbox"),DARK_MQ=window.matchMedia("(prefers-color-scheme: dark)");CHECKBOX.addEventListener("change",(function(e){e.target.checked?document.documentElement.setAttribute("data-theme","dark"):document.documentElement.setAttribute("data-theme","light")})),DARK_MQ.matches&&(CHECKBOX.checked=!0);
 //# sourceMappingURL=Script.js.map
--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,8 @@
 
 const CHECKBOX = document.getElementById('checkbox');
 
+const DARK_MQ = window.matchMedia("(prefers-color-scheme: dark)");
+
 CHECKBOX.addEventListener('change', (e) => {
  if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
@@ -9,3 +11,8 @@ CHECKBOX.addEventListener('change', (e) => {
       document.documentElement.setAttribute('data-theme', 'light');    
     }   
 });
+
+// check the toggle when prefers-color-scheme is dark
+if(DARK_MQ.matches) {
+  CHECKBOX.checked=true;
+}
